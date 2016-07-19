e5319196a42cd110691b02cb56998ee143ff063c
Ulf Kulau <kulau@ibr.cs.tu-bs.de>
2016-07-19 17:34:13 +0200

    script sending the commands to the specific nodes

--- apps/pdr-test/scripts/eval_director/eval-tx-multichannel.py
+++ apps/pdr-test/scripts/eval_director/eval-tx-multichannel.py
@@ -5,16 +5,20 @@ import testbedclient
 seqnrfile = '/tmp/tmp/pn-seqnr'
 broadcastdest = 65535
 
-idealVolting_active = 1
+#TODO Update the lists
+#usinga all combinatopns would take too much time
+txlevellist = [0, 1, 2, 3]
+channellist = [11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+nodelist = ['node0', 'node1', 'node2', 'node3', 'node4', 'node5', 'node6', 'node7', 'node8','node11','node12','node13','node14','node15','node16','node17','node18','node19']
 
-txlevellist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-nodelist = ['node0', 'node1', 'node2', 'node3', 'node4', 'node5', 'node6', 'node7', 'node8','node10','node11','node12','node13','node16','node17','node18']
+def send_rx_command(cli, node, channel):
+	cli.write(node, "rx d %d\n" % (channel))
 
-def transmit(cli, source, dest, power, data):
-	cli.write(source, "t %d %d %s" % (dest, power, data))
+def send_tx_command(cli, node, channel):
+	cli.write(source, "tx %d sweep\n" % (channel))
 
-def volting(cli, source, switch):
-	cli.write(source, "v %d" % (switch))
+def send_status_command(cli, node):
+	cli.write(node, "stat\n")
 
 if __name__ == "__main__":
 	seqnr = 0
@@ -29,28 +33,22 @@ if __name__ == "__main__":
 		print "file closed."
 		seqnr = 0
 
-		
+
 	tbc = testbedclient.testbedclient()
 #	for n in nodes:
 #		print n
 	while True:
 #		nodes = tbc.getnodes()
-		
-		for node in nodelist:
-			if idealVolting_active == 1:
-				volting(tbc, node, 0)		
-			else:
-				volting(tbc, node, 1)
-		time.sleep(1)
-		idealVolting_active ^=1
+		for channel in channellist:
+			for node in nodelist:
+				print "sending rx command to" + node + " with channel" + str(channel)
+				send_rx_command(tbc,node,channel);
 
-		for node in nodelist:
-			for power in txlevellist:
-				print "forcing " + node + " to transmit with power " + str(power) + "..."
-				transmit(tbc, node, broadcastdest, power, str(seqnr))
-				seqnr += 1
-				f = open(seqnrfile, "w")
-				f.write(str(seqnr)+"\n")
-				f.close
-				time.sleep(1)
-				
+			for node in nodelist:
+				print "sending tx command to" + node + " with channel" + str(channel)
+				send_tx_command(tbc,node,channel);
+				time.sleep(10)
+
+			for node in nodelist:
+				print "sending stat command to" + node
+				send_status_command(tbc,node);
